96c53f3ee51ab054fa867d3a6a87e1ebd139436e
Danilo Augusto <dasf_cm@terra.com.br>
2021-03-15 23:34:49 -0300

    feat: add comment

--- user.go
+++ user.go
@@ -1,5 +1,6 @@
 package data_integration
 
+// User represents User domain type
 type User struct {
 	ID       string `json:"id,omitempty" bson:"_id,omitempty"`
 	Email    string `json:"email" bson:"email"`
